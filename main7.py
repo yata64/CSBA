8b636c80a9b348c109cb056f208743f37e9a724b
yata64 <joaomarioys@gmail.com>
2025-06-09 11:49:39 -0300

    Finalização das funções
    feito as ultimas 3 funções: par/impar, maior e menor numero

--- main7.py
+++ main7.py
@@ -28,5 +28,58 @@ class Quadro(BoxLayout):
             else:
                 self.ids.resultado.text = str(o2-o1)
 
-        except ValueError: 
-            print("ERRO!!! Digite um número!")
+        except: 
+            self.ids.mensagem.text = \
+            "[color=#FF0000] Favor fornecer os operandos corretamente!![/color]"
+
+    def n_maior(self):
+        try: 
+            o1 = float(self.ids.op1.text)
+            o2 = float(self.ids.op2.text)
+            if o1 > o2:
+                self.ids.resultado.text = str(o1) 
+            
+            else: 
+                self.ids.resultado.text = str(o2)
+
+        except: 
+            self.ids.mensagem.text = \
+            "[color=#FF0000] Favor fornecer os operandos corretamente!![/color]"
+
+    def n_menor(self):
+        try: 
+            o1 = float(self.ids.op1.text)
+            o2 = float(self.ids.op2.text)
+            if o1 > o2:
+                self.ids.resultado.text = str(o2) 
+            
+            else: 
+                self.ids.resultado.text = str(o1)
+
+        except: 
+            self.ids.mensagem.text = \
+            "[color=#FF0000] Favor fornecer os operandos corretamente!![/color]"
+    
+    def par_impar(self):
+        try:
+            o1 = float(self.ids.op1.text)
+            o2 = float(self.ids.op2.text)
+            
+            if o1 % 2 == 0: 
+                self.ids.mensagem.text = str(o1)
+                "[color=#FF0000] é par!![/color]"
+
+            else: 
+                self.ids.mensagem.text = str(o1)
+                "[color=#FF0000] é impar!![/color]"
+            
+            if o2 % 2 == 0: 
+                self.ids.mensagem.text = str(o2)
+                "[color=#FF0000] é par!![/color]"
+
+            else: 
+                self.ids.mensagem.text = str(o2)
+                "[color=#FF0000] é impar!![/color]"
+        except:
+            self.ids.mensagem.text = \
+            "[color=#FF0000] Favor fornecer os operandos corretamente!![/color]"
